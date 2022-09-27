deb16a0186905c448dd0fe9d34e60773244b54b3
LFiosx <kurnosovaiv@mail.ru>
2022-09-27 14:35:10 +0300

    Add bds

--- Lab_2/main.py
+++ Lab_2/main.py
@@ -91,8 +91,47 @@ def iddfs (st, graph, check, res: list):
             print('addfl count: ' + str(i))
             return result
 
+
+def bds (queueS: deque, queueE: deque, graph, check, res = []):
+    left = queueS.popleft()
+    right = queueE.popleft()
+    if left == start:
+        check[left] = 0
+    if right == end:
+        check[right] = 0
+    if left in queueE:
+        res.insert(0, left)
+        return check
+    if right in queueS:
+        res.append(right)
+        return check
+    townsL = graph[left]
+    for x in townsL:
+        if x in queueE:
+            res.insert(0, x)
+            res.insert(0, left)
+            return check
+        if check[x] == -1:
+            check[x] = check[left] + 1
+            queueS.append(x)
+    townsR = graph[right]
+    for x in townsR:
+        if x in queueS:
+            res.append(right)
+            res.append(x)
+            return check
+        if check[x] == -1:
+            check[x] = check[right] + 1
+            queueE.append(x)
+    bds(queueS, queueE, graph, check, res)
+    if (res[0] in townsL) and (check[left] == check[res[0]] - 1):
+        res.insert(0, left)
+    if (res[len(res)-1] in townsR) and (check[right] == check[res[len(res)-1]] - 1):
+        res.append(right)
+    return res
+
+
 read_f('test.txt')
-fifo = deque([start])
 
 ch = visits.copy()
 print(bfs(deque([start]), graph, ch))
@@ -105,3 +144,6 @@ print(dls(start, graph, ch, [end], 6))
 print('\n********************************************************\n')
 ch = visits.copy()
 print(iddfs(start, graph, ch, [end]))
+print('\n********************************************************\n')
+ch = visits.copy()
+print(bds(deque([start]), deque([end]), graph, ch))
